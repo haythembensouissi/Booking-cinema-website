8f5e49d0e5509826700dd59251474f41bcc3a76a
Badre-Edin <dridibadr@gmail.com>
2022-07-14 15:17:33 +0100

    updated css

--- tonight-movies/src/components/LoginForm.vue
+++ tonight-movies/src/components/LoginForm.vue
@@ -1,14 +1,17 @@
 <template>
   <form>
-    <div class="form-group">
-      <label>Username</label>
-      <input class="form-control" placeholder="Enter your name" required v-model="Username"/>
-    </div>
-    <div class="form-group">
-      <label>Password</label>
-      <input class="form-control" type="password" placeholder="password" required v-model="Username" />
-    </div>
-    <router-link to="/HomePage" >Login</router-link>
+    <section>
+      <div class="form-group">
+        <label>Username</label><br />
+        <input class="form-control" placeholder="Enter your name" required v-model="Username" /><br />
+
+
+        <label>Password</label><br />
+        <input class="form-control" type="password" placeholder="password" required v-model="Username" /><br /> <br />
+      
+        <router-link to="/HomePage" class="button">Login</router-link>
+      </div>
+    </section>
   </form>
 </template>
 
@@ -22,18 +25,18 @@ export default defineComponent({
     Username: String,
     Password: String,
   },
-  data(){
-    return{
-      username:String,
-      password:String,
-      error:String,
-      
+  data() {
+    return {
+      username: String,
+      password: String,
+      error: String,
+
     }
   },
-  
+
   methods: {
-  
-}
+
+  }
 })
 
 
@@ -41,5 +44,51 @@ export default defineComponent({
 
 
 <style scoped>
+form {
+  text-align: center;
+  padding-top: 200px;
 
+
+}
+
+
+.button {
+  font-size: large;
+  width: 38.5%;
+  background-color: hwb(242 10% 68% / 0.747);
+  color: white;
+  padding: 14px 20px;
+
+  margin-left: -350px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  transition-duration: 0.2s;
+  position: absolute;
+}
+
+
+
+.button:hover {
+  background-color: rgba(165, 42, 42, 0.733);
+  color: white;
+}
+
+input {
+  width: 40%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+}
+
+label {
+  color: white;
+  font-size: large;
+
+  margin-right: auto;
+
+}
 </style>
---
+++ tonight-movies/src/views/WelcomePage.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    <h1 class="welcome">Welcome to tonight movies</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'WelcomePage',
+  components: {
+   
+  },
+});
+</script>
+
+<style>
+.welcome{
+  color: rgb(255, 174, 0);
+  text-align: center;
+  padding-top: 200px;
+  font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+  font-size: x-large;
+}
+</style>
