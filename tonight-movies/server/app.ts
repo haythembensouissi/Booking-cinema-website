ae040ba96bbf1acfa55085d27a7155c0a0905bc8
Badre-Edin <dridibadr@gmail.com>
2022-07-14 10:04:03 +0100

    updated server

--- tonight-movies/server/app.ts
+++ tonight-movies/server/app.ts
@@ -1,7 +1,19 @@
 import express, { Request, Response, NextFunction } from 'express';
+import cookieParser from 'cookie-parser';
+
 
 const app = express();
 const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
+
+app.get('/test',(req,res)=>{
+    
+    console.log('hello world');
+    
+})
 
 app.listen(port, () => {
   console.log(`Timezones by location application is running on port ${port}.`);
